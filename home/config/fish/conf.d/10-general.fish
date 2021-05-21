75c15145d4be280810a5793f65b759e388e8bdf2
Guilherme Tavares <guilherme.tsilva@gmail.com>
2021-05-21 03:24:32 -0300

    feat(variables): set BAT_STYLE variable

--- home/config/fish/conf.d/10-general.fish
+++ home/config/fish/conf.d/10-general.fish
@@ -1,5 +1,2 @@
 # Disable greeting
 set fish_greeting
-
-# Add user bin to PATH
-fish_add_path ~/.local/bin
---
+++ home/config/fish/conf.d/20-variables.fish
@@ -0,0 +1,7 @@
+# Add user bin to PATH
+fish_add_path ~/.local/bin
+
+# Bat style
+if type -q bat
+    set -gx BAT_STYLE "changes,numbers,snip"
+end
